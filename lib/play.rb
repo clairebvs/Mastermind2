1214289a9e98dcc5912e361b28da2cdd6f15cd84
ClaireBvs <clairembeauvais@gmail.com>
2018-04-10 12:25:22 -0600

    new test and function get_input

--- lib/play.rb
+++ lib/play.rb
@@ -1,14 +1,26 @@
 class Play
 
-def instruction
-  return "Welcome to MASTERMIND"
-  return "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-  user_input = ""
-  until user_input == "q"
+  def welcome_message
+   "Welcome to MASTERMIND, Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
+
+  def get_input
+    user_input = ""
     user_input = gets.chomp
-    if user_input == "p"
-      puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+    until user_input == "q" || user_input == "quit"
+      # user_input = gets.chomp
+      if user_input == "p" || user_input == "play"
+        puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+      else user_input == "i" || user_input == "quit"
+        puts "Try to find a sequence of four elements made up of red, blue, yellow or green. One color can be use as many times as you want. After each attempt you will know how many correct colors you have and how many in the correct position. "
+        user_input = gets.chomp
+      end
+      break
     end
+  end
+
 end
 
-# play = Play.new
+play = Play.new
+puts play.welcome_message
+puts play.get_input
--- test/play_test.rb
+++ test/play_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/play'
 require './lib/check_guess'
 require './lib/rand_sequence'
+require 'pry'
 
 class PlayTest < Minitest::Test
 
@@ -11,10 +12,19 @@ class PlayTest < Minitest::Test
     assert_instance_of Play, play
   end
 
-  def  test_it_has_instruction
+  def  test_it_has_welcome_message
     play = Play.new
-    assert_equal "Welcome to MASTERMIND", play.instruction
-
+    assert_equal "Welcome to MASTERMIND, Would you like to (p)lay, read the (i)nstructions, or (q)uit?", play.welcome_message
   end
+
+  def test_it_gets_user_input
+    play = Play.new
+    user_input = "i"
+    actual = Play.new.get_input
+    expected = "Try to find a sequence of four elements made up of red, blue, yellow or green. One color can be use as many times as you want. After each attempt you will know how many correct colors you have and how many in the correct position."
+    assert_equal expected, actual
+  end
+
+
 
 end
